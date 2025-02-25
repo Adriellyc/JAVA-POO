31b9370ee2a1f486223a9e0e84a55a8a5c1a7a98
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-02-25 22:57:47 +0000

    poo

---
+++ EX1/Professor.java
@@ -0,0 +1,7 @@
+public class Professor{
+public String nome; 
+public double altuta;
+public String nacionalidade;
+    
+}
+ 
---
+++ EX1/Universidade.java
@@ -0,0 +1,6 @@
+public class Universidade{
+    public String none; 
+    public String cidade;
+    public String estado;
+    }
+        
